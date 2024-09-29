c35edb98d05b1b24b9cc20a678e50eaa3dbc2c0a
kalashnikovpi <kalashnicovp@yandex.ru>
2024-09-29 16:32:22 +0500

    Add project files.

---
+++ Tyuiu.KalashnikovPI.Sprint1.Task0.V5.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KalashnikovPI.Sprint1.Task0.V5.Lib
+{
+    public class DataService : ISprint1Task0V5
+    {
+        public double Calculate()
+        {
+            return (1 + 2) * (1 + 9 / 3);
+        }
+    }
+}
---
+++ Tyuiu.KalashnikovPI.Sprint1.Task0.V5/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KalashnikovPI.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.KalashnikovPI.Sprint1.Task0.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Калашников П. И. | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт 1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы C#                                          *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Калашников Павел Ильич | СМАРТб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (1+2)*(1+9/3)           *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫНЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  (1+2)*(1+9/3)                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
